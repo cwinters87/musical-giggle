95d247ae7217d182f698f8509f44f93bdd89b700
Chris Winters <chris@tasksuite.com>
2023-10-02 13:42:33 -0500

    updated testimonials

--- src/components/homePage/testimonial/testimonialCard.js
+++ src/components/homePage/testimonial/testimonialCard.js
@@ -6,8 +6,15 @@ import * as styles from "./testimonialCard.module.css"
 const cardData = [
   {
     imageUrl: backgroundImg,
+    quote: `Mainati Alternate Student Funding has been using TaskFlow for our call centre and CRM solutions as well as loan and accounting modules. TaskFlow is an enabler to our business, and we are envisioning a long and frutiful relationship.`,
+    author: "Paul Bruwer",
+    position: "CFO",
+    company: "MANATI",
+  },
+  {
+    imageUrl: backgroundImg,
     quote:
-      "We’ve worked with TaskSuite to develop a customized product that is tailored to our exact business needs. They’ve done this seamlessly",
+      "We’ve worked with TaskFlow to develop a customized product that is tailored to our exact business needs. They’ve done this seamlessly",
     author: "Tucker S.",
     position: "COO",
     company: "BACKD",
@@ -15,17 +22,10 @@ const cardData = [
   {
     imageUrl: backgroundImg,
     quote:
-      "TaskSuite has provided ongoing support and valuable insight as to how we can continue to utilize these tools to increase productivity and operate our business efficiently.",
+      "TaskFlow has provided ongoing support and valuable insight as to how we can continue to utilize these tools to increase productivity and operate our business efficiently.",
     author: "Brandon Hoye",
     position: "CTO",
     company: "TUA FINANCIAL",
-  },
-  {
-    imageUrl: backgroundImg,
-    quote: `We've been working with the TaskSuite team for over three years. They provide our business with the best tools in the industry. I am impressed with their product and their capabilities.`,
-    author: "Tom Sultenfuss",
-    position: "FOUNDER",
-    company: "CONTACTWORKS",
   },
   // Add more card data objects as needed
 ]
